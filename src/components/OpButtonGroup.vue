b9bc1bf603a269dc1f379249d2d562959bda2193
comphonia <37687869+comphonia@users.noreply.github.com>
2022-02-03 23:08:46 -0600

    designed the base layout and components

---
+++ src/components/OpButtonGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class="vqb-btn_group">
+    <VBtn class="selected">Any</VBtn>
+    <VBtn>All</VBtn>
+  </span>
+</template>
+
+<script>
+import { VBtn } from 'vuetensils/src/components'
+
+export default {
+  name: 'OpButtonGroup',
+  data() {
+    return {
+      option: `any`,
+    }
+  },
+  components: {
+    VBtn,
+  },
+}
+</script>
+
+<style>
+  .vqb-btn_group > .vts-btn{
+    padding: 5px 8px;
+    margin: 0 2px;
+    cursor: pointer;
+    border-style:solid;
+    border-color: var(--vqb-main-color);
+    font-weight: 600;
+    color: #444;
+  }
+  .vqb-btn_group > .vts-btn.selected{
+    color: #fff;
+    background-color: var(--vqb-main-color);
+  }
+</style>
---
+++ src/components/QueryBuilder.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="vqb-container">
+    <div class="vqb-option_box">Results must match <OpButtonGroup /> of the following rules:</div>
+    <div class="vqb-rule-creator">
+      <div class="vqb-content">
+        New Rule: 
+        <VInput type="select" :options='options' :value='options[0]' style='margin: 0 15px' /> 
+        <VInput type="select" :options='options2' :value='options2[0]' style='margin-right: 15px' /> 
+        <VInput type="text" style='margin-right: 15px' /> 
+        <VBtn>+ Add Rule</VBtn>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VBtn, VInput } from 'vuetensils/src/components'
+import OpButtonGroup from './OpButtonGroup.vue'
+
+export default {
+  name: 'QueryBuilder',
+
+  data() {
+    return {
+      options: ['FirstName', 'LastName', 'Age'],
+      options2: ['equals', 'does not equal', 'contains'],
+    }
+  },
+
+  components: {
+    OpButtonGroup,
+    VBtn,
+    VInput
+  },
+}
+</script>
+
+<style>
+.vqb-container {
+  --vqb-main-color: #41b883;
+  min-width: 350px;
+  max-width: 100%;
+  border: 1px solid #ccc;
+  padding-bottom: 16px;
+  margin: auto;
+  font-size: 1.2rem;
+  color: #444;
+}
+.vqb-container .vts-btn, .vqb-container .vts-input__input, .vqb-container .vts-input__input{
+  padding: 5px 8px;
+  margin: 0 2px;
+  cursor: pointer;
+  border-style: solid;
+  border-color: var(--vqb-main-color);
+  font-weight: 600;
+  color: var(--vqb-main-color);
+  background-color: inherit;
+}
+.vqb-container .vts-input__input{
+  cursor: inherit;
+}
+.vqb-option_box {
+  background-color: #f5f5f5;
+  border-left: 3px solid var(--vqb-main-color);
+  padding: 8px 16px;
+}
+.vqb-content {
+  padding: 16px;
+}
+.vqb-rule-creator {
+  font-size: 1rem;
+  border-top: 1px dashed #ccc;
+  border-bottom: 1px dashed #ccc;
+}
+.vqb-rule-creator > .vts-btn {
+  background-color: var(--vqb-main-color);
+  color: #fff;
+}
+.vqb-rule-creator > .vqb-content {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+</style>
